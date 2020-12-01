69439bc41882e83a49e1f93f8de6d3dc5f366306
Ignacio SV <ignaciosanchezvilar@gmail.com>
2020-12-01 19:47:52 -0300

    1er commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nombre = prompt('Ingrese su nombre');
+
+alert('Hola ' + nombre + '!');
+
+let apellido = prompt('Ahora ingrese su apellido');
+
+alert('Mire la consola');
+
+console.log('Hola ' + nombre + ' ' + apellido);
